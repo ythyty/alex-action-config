30456846968470197b5ddacfb1e356bf5d338d05
ythyty <44136048+ythyty@users.noreply.github.com>
2021-05-13 16:48:03 +0800

    1111

--- lean-lede/prenv.sh
+++ lean-lede/prenv.sh
@@ -12,6 +12,8 @@ docker system prune --all --force --volumns
 # sudo -E apt-get -qq -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php* docker*
 sudo -E apt-get -qq update
 sudo -E apt-get -qq install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync libreadline-dev ccache vim nano python python-pip python3-pip python-ply python3-ply haveged lrzsz scons ecj fastjar mkisofs
+sudo -E apt-get -qq install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint
+
 sudo -E apt-get -qq autoremove --purge
 sudo -E apt-get -qq clean
 sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
